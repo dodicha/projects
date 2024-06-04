c4e5c6a4bdb7bc195502abd1b95ab41114b62011
gogicha <g.dodicha@gmail.com>
2024-06-05 03:48:34 +0400

    finished Sign Up functuality

--- service/src/components/SignUp.tsx
+++ service/src/components/SignUp.tsx
@@ -1,4 +1,6 @@
 import { User } from "./users";
+import { useRef } from "react";
+
 function checkemail(string:string){
   const end: string | undefined= string.slice(-10);
   return end === '@gmail.com';
@@ -9,12 +11,23 @@ function checkPassword(string:string){
   const symbol = /[!@#$%^&*(),.?":{}|<>]/.test(string);
   return number && upperCase && symbol
 };
-function clearValue(email: HTMLInputElement, password: HTMLInputElement, firstname: HTMLInputElement, lastName: HTMLInputElement ){
-  email.value = '';
-  password.value = '';
-  firstname.value = '';
-  lastName.value = '';
+function clearValue(email: HTMLInputElement, password: HTMLInputElement, firstname: HTMLInputElement, lastName: HTMLInputElement){
+    email.value = '';
+    password.value = '';
+    firstname.value = '';
+    lastName.value = '';
 }
+function clearErr(email: HTMLElement | null, password: HTMLElement | null){
+  if(email){
+    email.style.display = 'none';
+  }
+  if(password){
+    password.style.display = 'none';
+  }
+}
+
+    
+
 
 export default function SignUp(props: {
   showSignUp: string,
@@ -25,31 +38,56 @@ export default function SignUp(props: {
   user: any
   }  ){
 
-    const firstname = document.getElementById('firstName') as HTMLInputElement | null;
-    const lastName = document.getElementById('lastName') as HTMLInputElement | null;
-    const email = document.getElementById('email') as HTMLInputElement | null;
-    const password = document.getElementById('password') as HTMLInputElement | null;
+    const firstname = document.getElementById('firstName') as HTMLInputElement;
+    const lastName = document.getElementById('lastName') as HTMLInputElement;
+    const email = document.getElementById('email') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
+    const formInputs = [firstname, lastName, email, password];
 
-  function handleClick(){
-    if(email?.value && password?.value && firstname?.value && lastName?.value){
-      if(checkemail(email?.value) && checkPassword(password?.value)){
+    const emailErr: HTMLElement | null= document.getElementById('email-err');
+    const passErr: HTMLElement | null = document.getElementById('password-err');
+
+
+
+    function handleSignUp(){
         props.setShowSignUp('none');
         props.setCount(props.count + 1);
         props.users[props.count] = new props.user(firstname.value, lastName.value, email.value, password.value);
-        clearValue(email, password, firstname, lastName);
         props.setShowSignUp('none');
-      }
-      if(!checkemail(email?.value) && email?.value !== ''){
-        const emailErr: HTMLElement | null= document.getElementById('email-err');
+    }
+    function handleEmailErr(){  
         if(emailErr){
-          emailErr.style.display = 'block'
+          emailErr.style.display = 'block'  
         }
+    }
+    function handlePassErr(){
+      if(passErr){
+        passErr.style.display = 'block';
       }
-      if(!checkPassword(password?.value) && password?.value !== ''){
-        const passErr: HTMLElement | null = document.getElementById('password-err');
-        if(passErr){
-          passErr.style.display = 'block';
+    }
+    function handleEmptyValues(){
+      formInputs.map((e, index) => {
+        if(e){
+          e?.value === '' ? e.style.border = '2px solid red' : e.style.border = '2px solid black'
         }
+      })
+    }
+
+  function handleSignClick(){
+
+    handleEmptyValues();
+    
+    if(email?.value && password?.value && firstname?.value && lastName?.value){
+      if(checkemail(email.value) && checkPassword(password.value)){
+        handleSignUp();
+        clearValue(email, password, firstname, lastName);
+        clearErr(emailErr, passErr)
+      }
+      if(!checkemail(email.value) && email.value !== ''){
+        handleEmailErr();
+      }
+      if(!checkPassword(password.value) && password.value !== ''){
+        handlePassErr();
       }
     }
   }
@@ -60,7 +98,11 @@ export default function SignUp(props: {
                 <div className='head'>
                   <h1>Sign Up</h1>
                   <button 
-                  onClick={()=>props.setShowSignUp('none')}
+                  onClick={()=>{
+                    props.setShowSignUp('none');
+                    clearValue(email, password, firstname, lastName);
+                    clearErr(emailErr, passErr)
+                  }}
                   >&times;</button>
                 </div>
                 <hr style={{color: 'black'}}></hr>
@@ -76,7 +118,7 @@ export default function SignUp(props: {
                 </div>
                 <button 
                 id='signUp'
-                onClick={handleClick}
+                onClick={handleSignClick}
                 >Sign Up</button>
             </div>
           </div>
